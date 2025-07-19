9b703ce8539e0fd8ecab504169aecf738bd4f171
Sandip Chaudhary <chaudharysandip124@gmail.com>
2025-07-19 12:51:43 +0200

    chore: add tests to verify the month name combination

--- tests/NepDate.Tests/Core/SmartDateParserTests.cs
+++ tests/NepDate.Tests/Core/SmartDateParserTests.cs
@@ -174,4 +174,157 @@ public class SmartDateParserTests
         Assert.True(success);
         Assert.Equal(expectedDate, result);
     }
+    
+    [Theory]
+    // Baisakh (1)
+    [InlineData("baisakh", 1)]
+    [InlineData("baishakh", 1)]
+    [InlineData("baisak", 1)]
+    [InlineData("vaisakh", 1)]
+    [InlineData("vaisakha", 1)]
+    [InlineData("vaishak", 1)]
+    [InlineData("vaisakhi", 1)]
+    [InlineData("baishak", 1)]
+    [InlineData("baisaga", 1)]
+    [InlineData("baishaga", 1)]
+
+    // Jestha (2)
+    [InlineData("jestha", 2)]
+    [InlineData("jeth", 2)]
+    [InlineData("jeshtha", 2)]
+    [InlineData("jyeshtha", 2)]
+    [InlineData("jyestha", 2)]
+    [InlineData("jesth", 2)]
+    [InlineData("jeshth", 2)]
+    [InlineData("jetha", 2)]
+    [InlineData("jeshta", 2)]
+    [InlineData("jayshtha", 2)]
+    [InlineData("jayestha", 2)]
+    [InlineData("jesta", 2)]
+    [InlineData("jyesth", 2)]
+    [InlineData("jyaistha", 2)]
+    [InlineData("jaistha", 2)]
+
+    // Asar (3)
+    [InlineData("asar", 3)]
+    [InlineData("asadh", 3)]
+    [InlineData("ashar", 3)]
+    [InlineData("ashad", 3)]
+    [InlineData("asad", 3)]
+    [InlineData("aasad", 3)]
+    [InlineData("asada", 3)]
+    [InlineData("ashadh", 3)]
+    [InlineData("asadha", 3)]
+    [InlineData("ashadha", 3)]
+    [InlineData("ashara", 3)]
+    [InlineData("asara", 3)]
+    [InlineData("ashada", 3)]
+    [InlineData("asaad", 3)]
+    [InlineData("aashar", 3)]
+
+    // Shrawan (4)
+    [InlineData("shrawan", 4)]
+    [InlineData("sawan", 4)]
+    [InlineData("saun", 4)]
+    [InlineData("srawan", 4)]
+    [InlineData("shraawan", 4)]
+    [InlineData("shravan", 4)]
+    [InlineData("shravana", 4)]
+    [InlineData("sawun", 4)]
+    [InlineData("savan", 4)]
+    [InlineData("shrawana", 4)]
+    [InlineData("sravana", 4)]
+    [InlineData("sawon", 4)]
+    [InlineData("sravan", 4)]
+    [InlineData("saawan", 4)]
+    [InlineData("sharwan", 4)]
+    [InlineData("sarwan", 4)]
+    [InlineData("sraawan", 4)]
+    [InlineData("shaun", 4)]
+    [InlineData("shawan", 4)]
+
+    // Bhadra (5)
+    [InlineData("bhadra", 5)]
+    [InlineData("bhadrapad", 5)]
+    [InlineData("bhadrapada", 5)]
+    [InlineData("bhadra pad", 5)]
+    [InlineData("bhadraw", 5)]
+    [InlineData("bhadar", 5)]
+    [InlineData("bhadrapaksh", 5)]
+    [InlineData("bhdra", 5)]
+    [InlineData("bhadarwa", 5)]
+    [InlineData("bhadrawa", 5)]
+
+    // Ashoj (6)
+    [InlineData("ashoj", 6)]
+    [InlineData("asoj", 6)]
+    [InlineData("ashwin", 6)]
+    [InlineData("ashvina", 6)]
+    [InlineData("ashwayuja", 6)]
+    [InlineData("asuj", 6)]
+    [InlineData("asoja", 6)]
+    [InlineData("ashvayuja", 6)]
+    [InlineData("ashwinak", 6)]
+
+    // Kartik (7)
+    [InlineData("kartik", 7)]
+    [InlineData("kartika", 7)]
+    [InlineData("karthik", 7)]
+    [InlineData("karttika", 7)]
+    [InlineData("kaartik", 7)]
+    [InlineData("karthika", 7)]
+    [InlineData("kaarthik", 7)]
+    [InlineData("kartic", 7)]
+
+    // Mangsir (8)
+    [InlineData("mangsir", 8)]
+    [InlineData("mangsirh", 8)]
+    [InlineData("mangsar", 8)]
+    [InlineData("mangsira", 8)]
+    [InlineData("mangsire", 8)]
+    [InlineData("mangsira", 8)]
+    [InlineData("margashirsha", 8)]
+    [InlineData("margashira", 8)]
+    [InlineData("margshirsha", 8)]
+    [InlineData("margsheersh", 8)]
+    [InlineData("mangsira", 8)]
+
+    // Poush (9)
+    [InlineData("poush", 9)]
+    [InlineData("paush", 9)]
+    [InlineData("pousha", 9)]
+    [InlineData("pausha", 9)]
+    [InlineData("push", 9)]
+    [InlineData("pusha", 9)]
+    [InlineData("paus", 9)]
+    [InlineData("pous", 9)]
+
+    // Magh (10)
+    [InlineData("magh", 10)]
+    [InlineData("magha", 10)]
+    [InlineData("mag", 10)]
+    [InlineData("maagh", 10)]
+    [InlineData("maagha", 10)]
+
+    // Falgun (11)
+    [InlineData("falgun", 11)]
+    [InlineData("phagun", 11)]
+    [InlineData("phalgun", 11)]
+    [InlineData("phalguna", 11)]
+    [InlineData("faagun", 11)]
+    [InlineData("phalguni", 11)]
+    [InlineData("fagun", 11)]
+
+    // Chaitra (12)
+    [InlineData("chaitra", 12)]
+    [InlineData("chait", 12)]
+    [InlineData("chaitri", 12)]
+    [InlineData("chaitram", 12)]
+    [InlineData("chaitanya", 12)]
+    [InlineData("chaitarah", 12)]
+    public void Parse_WithNepaliMonthName_ReturnsCorrectDate(string input, int expectedMonth)
+    {
+        var expectedDate = new NepaliDate(2080, expectedMonth, 15);
+        Assert.Equal(expectedDate, SmartDateParser.Parse($"15 {input} 2080"));
+    }
 }
